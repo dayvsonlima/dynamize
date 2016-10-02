18ad0b6581d04664ad447bd84acc599ab56a12fb
Dayvson Lima <dayvsonlima31@gmail.com>
2016-10-02 00:56:06 -0300

    fix: include of rake task

--- lib/tasks/migrate.rb
+++ lib/tasks/migrate.rb
@@ -2,7 +2,7 @@ namespace :dy do
   desc "run dynamo migrations"
   task "migrate" => :environment do
   	puts 'implements...'
-    # DynamoDB::Migration.run_all_migrations(options)
+    DynamoDB::Migration.run_all_migrations(Dynamize::DYNAMODB_OPTIONS)
   end
 end
 # Rails.env
